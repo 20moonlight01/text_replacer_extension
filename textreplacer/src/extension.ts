b1852c0d17da5fbb9fad817680096249219ab10f
20moonlight01 <nadezdaglebova06@gmail.com>
2024-11-06 21:38:01 +0300

    replace text command added

--- textreplacer/src/extension.ts
+++ textreplacer/src/extension.ts
@@ -1,26 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const keyMap: { [key: string]: string } = {
+    'a': 'ф', 'b': 'и', 'c': 'с', 'd': 'в', 'e': 'у', 'f': 'а',
+    'g': 'п', 'h': 'р', 'i': 'ш', 'j': 'о', 'k': 'л', 'l': 'д',
+    'm': 'ь', 'n': 'т', 'o': 'щ', 'p': 'з', 'q': 'й', 'r': 'к',
+    's': 'ы', 't': 'е', 'u': 'г', 'v': 'м', 'w': 'ц', 'x': 'ч',
+    'y': 'н', 'z': 'я', '[': 'х', ']': 'ъ', '`': 'ё', ',': 'б',
+    '.': 'ю', ';': 'ж', "'": 'э',
+    'A': 'Ф', 'B': 'И', 'C': 'С', 'D': 'В', 'E': 'У', 'F': 'А',
+    'G': 'П', 'H': 'Р', 'I': 'Ш', 'J': 'О', 'K': 'Л', 'L': 'Д',
+    'M': 'ь', 'N': 'Т', 'O': 'Щ', 'P': 'З', 'Q': 'Й', 'R': 'К',
+    'S': 'ы', 'T': 'Е', 'U': 'Г', 'V': 'М', 'W': 'Ц', 'X': 'Ч',
+    'Y': 'Н', 'Z': 'Я', '{': 'Х', '}': 'ъ', '~': 'Ё', '<': 'Б',
+    '>': 'Ю', ':': 'Ж', '"': 'Э'
+};
+
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('textreplacer.replaceText', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const selection = editor.selection;
+            const selectedText = editor.document.getText(selection);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "textreplacer" is now active!');
+            const replacedText = selectedText.split('').map(char => keyMap[char] || char).join('');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('textreplacer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from TextReplacer!');
-	});
+            editor.edit(editBuilder => {
+                editBuilder.replace(selection, replacedText);
+            });
+        }
+    });
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
